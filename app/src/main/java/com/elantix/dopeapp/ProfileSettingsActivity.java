517bf207fcacabeae99c7e6b8802123920d2e207
Oleh Li <dannogo@gmail.com>
2016-04-05 02:24:22 +0300

    Just backup

---
+++ app/src/main/java/com/elantix/dopeapp/ProfileSettingsActivity.java
@@ -0,0 +1,16 @@
+package com.elantix.dopeapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by oleh on 4/4/16.
+ */
+public class ProfileSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_settings);
+    }
+}
